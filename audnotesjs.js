70ee6cf2b5760c68a5ff2dd4a5e9932338cd7aac
no1snoopyfan <141663073+no1snoopyfan@users.noreply.github.com>
2023-08-08 00:17:56 -0500

    Update audnotesjs.js

--- audnotesjs.js
+++ audnotesjs.js
@@ -1,1 +1,26 @@
-const pi = 3.14
+const registerButton = document.getElementById('registerButton');
+
+registerButton.addEventListener('click', async () => {
+  const username = 'exampleuser';
+  const password = 'examplepassword';
+
+  try {
+    const response = await fetch('/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data.message);
+    } else {
+      const errorData = await response.json();
+      console.error(errorData.error);
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+});
